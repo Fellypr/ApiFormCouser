f87f7dc8a9db894a4f1c4fe54f99c061ef16ac2f
fellypr <fellype29kennedemil@gmail.com>
2025-06-24 15:03:25 -0300

    Ajusta versão do Microsoft.AspNetCore.OpenApi para 8.0 para compatibilidade

--- obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e63f88091c5203bbcd36ac8da82b6b6b83676681")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf00229c0c23e5f2a2692f7b5a91fea0a2b638a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
